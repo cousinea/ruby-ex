fef50b9b2eb2384506f28fdf92815c040357d3c9
chriskretler <ckretler@umich.edu>
2016-04-18 18:59:35 -0400

    now include require file.

--- my_app.rb
+++ my_app.rb
@@ -3,18 +3,14 @@ class MyApp
 	    [200, {"Content-Type" => "text/html"}, [<<WELCOME_CONTENTS
 <!doctype html>
 <html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <title>OpenShift Demo Application</title>
-
-</head>
 <body>
 
 <section class='container'>
           <hgroup>
             <h1>This sample ruby application demonstrates OpenShift's ability to build and deploy on git pushes.</h1>
           </hgroup>
+</section>
+
 </body>
 </html>	
 WELCOME_CONTENTS    
