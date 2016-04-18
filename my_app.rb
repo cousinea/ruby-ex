33765ced2bd206f0f6e5f3cc80677d662244091f
chriskretler <ckretler@umich.edu>
2016-04-18 18:48:10 -0400

    now include require file.

--- my_app.rb
+++ my_app.rb
@@ -1,5 +1,22 @@
 class MyApp
 	def call env
-	    [200, {"Content-Type" => "text/html"}, ["Hello rack and stack Participants"]]
+	    [200, {"Content-Type" => "text/html"}, [
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title>OpenShift Demo Application</title>
+
+</head>
+<body>
+
+<section class='container'>
+          <hgroup>
+            <h1>This sample ruby application demonstrates OpenShift's ability to build and deploy on git pushes.</h1>
+          </hgroup>
+</body>
+</html>	    
+	    ]]
 	end
 end
