ce0fd734a1f75a7bf720d428820c2c5b7c86631b
chriskretler <ckretler@umich.edu>
2016-04-18 19:02:07 -0400

    simplify html for debugging.

--- my_app.rb
+++ my_app.rb
@@ -1,19 +1,12 @@
 class MyApp
 	def call env
-	    [200, {"Content-Type" => "text/html"}, [<<WELCOME_CONTENTS
+	    [200, {"Content-Type" => "text/html"}, [
 <!doctype html>
 <html lang="en">
-<body>
-
-<section class='container'>
-          <hgroup>
-            <h1>This sample ruby application demonstrates OpenShift's ability to build and deploy on git pushes.</h1>
-          </hgroup>
-</section>
-
-</body>
+	<body>
+		<h1>This sample ruby application demonstrates OpenShift's ability to build and deploy on git pushes.</h1>
+	</body>
 </html>	
-WELCOME_CONTENTS    
 	    ]]
 	end
 end
