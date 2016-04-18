3e89cf286dbc0b175036bcd9af59eb09546829ee
chriskretler <ckretler@umich.edu>
2016-04-18 18:42:56 -0400

    now include require file.

--- my_app.rb
+++ my_app.rb
@@ -1,6 +1,6 @@
 # my_app.rb
 class MyApp
 	def call env
-	    [200, {"Content-Type" => "text/html"}, ["Hello Rack Participants"]]
+	    [200, {"Content-Type" => "text/html"}, ["Hello rack and stack Participants"]]
 	end
 end
